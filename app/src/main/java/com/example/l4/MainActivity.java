b852bcc549c15f8eaf35e17c438d212dd4542e8d
Likeprodingo <alshiba1@mail.ru>
2021-05-04 18:07:49 +0300

    Base

---
+++ app/src/main/java/com/example/l4/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.l4;
+
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.graphics.Point;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Display;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ScrollView;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/l4/OnBackPressed.java
@@ -0,0 +1,5 @@
+package com.example.l4;
+
+interface OnBackPressed {
+    void onBackPressed();
+}
